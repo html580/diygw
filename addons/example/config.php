1ecc726009646f0fca8378e2520320fd39fffce6
html580 <html580@126.com>
2019-04-25 11:40:54 +0800

    更新

--- addons/example/config.php
+++ addons/example/config.php
@@ -1,18 +1,18 @@
-						<?php
-						return [
-                            'title'=>[//配置在表单中的键名 ,这个会是config[title]
-                                'title'=>'显示标题:',//表单的文字
-                                'type'=>'text',		 //表单的类型：text、textarea、checkbox、radio、select等
-                                'value'=>'测试例子',			 //表单的默认值
-                            ],
-							'display'=>[//配置在表单中的键名 ,这个会是config[display]
-								'title'=>'是否显示:',//表单的文字
-								'type'=>'radio',		 //表单的类型：text、textarea、checkbox、radio、select等
-								'options'=>[		 //select 和radion、checkbox的子选项
-									'1'=>'显示',		 //值=>文字
-									'0'=>'不显示',
-								],
-								'value'=>'1',			 //表单的默认值
-							],
-						];
+<?php
+return [
+    'title' => [//配置在表单中的键名 ,这个会是config[title]
+        'title' => '显示标题:',//表单的文字
+        'type' => 'text',         //表单的类型：text、textarea、checkbox、radio、select等
+        'value' => '测试例子',             //表单的默认值
+    ],
+    'display' => [//配置在表单中的键名 ,这个会是config[display]
+        'title' => '是否显示:',//表单的文字
+        'type' => 'radio',         //表单的类型：text、textarea、checkbox、radio、select等
+        'options' => [         //select 和radion、checkbox的子选项
+            '1' => '显示',         //值=>文字
+            '0' => '不显示',
+        ],
+        'value' => '1',             //表单的默认值
+    ],
+];
 					
